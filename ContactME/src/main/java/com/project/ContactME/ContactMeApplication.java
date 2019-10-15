9df05263b993f7f98a0f51cbcf21c1da650e2f1d
jazperez <jackkiey24@gmail.com>
2019-10-14 17:53:56 -0700

    Add Spring project

---
+++ ContactME/src/main/java/com/project/ContactME/ContactMeApplication.java
@@ -0,0 +1,13 @@
+package com.project.ContactME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMeApplication.class, args);
+	}
+
+}
