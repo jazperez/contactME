6d226f415ddc70b01d4110a73677fcdf51305e45
jazperez <jackkiey24@gmail.com>
2019-12-10 13:05:03 -0800

    Fix Contact model and edit WorkContractControler

--- MyContacts/src/main/java/com/project/MyContacts/Contact.java
+++ MyContacts/src/main/java/com/project/MyContacts/Contact.java
@@ -14,9 +14,6 @@ public class Contact {
 	@GeneratedValue
     private Long id;
     private @NonNull String name;
-	public void setName(String name) {
-        this.name = name;
-	}
 	public Object getName() {
 		return name;
 	}
