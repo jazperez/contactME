fe287cc644d1c651a6022d951a0c55bc038b6580
jazperez <jackkiey24@gmail.com>
2019-12-10 17:30:05 -0800

    Fix CORS config and add unstaged files

--- MyContacts/src/main/java/com/project/MyContacts/ContactRepository.java
+++ MyContacts/src/main/java/com/project/MyContacts/ContactRepository.java
@@ -3,6 +3,9 @@ package com.project.MyContacts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
 interface ContactRepository extends JpaRepository<Contact, Long> {
 }
--- MyContacts/src/main/java/com/project/MyContacts/MyContactsApplication.java
+++ MyContacts/src/main/java/com/project/MyContacts/MyContactsApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import java.util.stream.Stream;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Collections;
+
 @SpringBootApplication
 public class MyContactsApplication {
 
@@ -23,9 +30,22 @@ public class MyContactsApplication {
                 		contact.setName(name);
 						repository.save(contact);
 						
-        });
+        	});
             repository.findAll().forEach(System.out::println);
-	};
-}
-		
+		};
+	}
+	
+	@Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
